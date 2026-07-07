a2028571a1fdd68f2d9f7e0f8e3bca66fde457df
Naushadi8 <59287926+Naushadi8@users.noreply.github.com>
2026-07-07 20:18:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,13 @@ my_dataframe = (
     .table("smoothies.public.fruit_options")
     .select(col("FRUIT_NAME"), col('SEARCH_ON')
 ))
-st.dataframe(data=my_dataframe, use_container_width = True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width = True)
+#st.stop()
 
-# Convert Snowflake dataframe to pandas/list
-fruit_df = my_dataframe.to_pandas()
+# Convert the Snowpark DataFrame to a pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 fruit_options = fruit_df["FRUIT_NAME"].tolist()
 
 # Fruit multiselect
