dc6b57d877e5b0f5fdd78d4076597498bb84a63c
Naushadi8 <59287926+Naushadi8@users.noreply.github.com>
2026-07-07 20:14:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,10 @@ st.write("The name on your Smoothie will be:", name_on_order)
 my_dataframe = (
     session
     .table("smoothies.public.fruit_options")
-    .select(col("FRUIT_NAME"))
+    .select(col("FRUIT_NAME"), col('SEARCH_ON')
 )
+st.dataframe(data=my_dataframe, use_container_width = True)
+st.stop()
 
 # Convert Snowflake dataframe to pandas/list
 fruit_df = my_dataframe.to_pandas()
